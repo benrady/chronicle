e17b43067449dc716f707f3a11061781749e4538
Ben Rady <benrady@gmail.com>
2012-05-06 15:22:46 -0500

    Updated specs for chronicle

--- spec/chronicle_spec.rb
+++ spec/chronicle_spec.rb
@@ -1,8 +1,29 @@
 require 'chronicle'
 
 describe Chronicle do
+  let (:generator) { double 'generator' }
+
+  before( :each ) do
+    Generator.stub(:new).and_return generator
+  end
+
   it "Starts a GUI" do
-    GUI.should_receive :new
+    GUI.should_receive(:new).with(generator)
     Chronicle.start
+  end
+
+  it "loads a roster if provided as args" do
+    GUI.stub :new
+    generator.should_receive(:load_roster).with("foo.csv")
+
+    Chronicle.start("foo.csv")
+  end
+
+  it "loads a sheet if provided as args" do
+    GUI.stub :new
+    generator.stub :load_roster
+    generator.should_receive(:load_sheet).with("scenario.png")
+
+    Chronicle.start("foo.csv", "scenario.png")
   end
 end
